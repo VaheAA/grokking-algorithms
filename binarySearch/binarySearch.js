712d55ea2fac79937cb58101698e8b2b42f5d08f
Vahe Abovyan <vahe.abov@gmail.com>
2023-03-28 14:35:02 +0400

    update binary search

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,18 +1,25 @@
 // Binary search - divide and conquer
 
 function binarySearch(list, item) {
+  // starting index
   let low = 0;
+  // highest index
   let high = list.length - 1;
 
+  // iterate while the array is not narrowed to one element
   while (low <= high) {
+    // check the middle index
     const mid = Math.floor((low + high) / 2);
     const guess = list[mid];
 
     if (guess === item) {
+      // the target found
       return mid;
     } else if (guess > item) {
+      // target is too high
       high = mid - 1;
     } else {
+      // target is too low
       low = mid + 1;
     }
   }
